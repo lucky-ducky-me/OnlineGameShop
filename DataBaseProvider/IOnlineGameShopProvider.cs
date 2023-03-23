7b879ae843e8be0288b6de5cd640c45c13a5ad7e
lucky-ducky-me <vlad01ovchinnikov02@gmail.com>
2023-03-23 11:16:20 +0300

    Добавлен интерфейс провайдера БД для магазина.

---
+++ DataBaseProvider/IOnlineGameShopProvider.cs
@@ -0,0 +1,22 @@
+﻿using DataBaseProvider.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseProvider
+{
+    public interface IOnlineGameShopProvider
+    {
+        public IEnumerable<User> GetAllUsers();
+
+        public IEnumerable<Genre> GetAllGenres();
+
+        public IEnumerable<Game> GetAllGames();
+
+        public IEnumerable<Order> GetAllOrders();
+
+        public IEnumerable<UserScore> GetAllUsersScore();
+    }
+}
