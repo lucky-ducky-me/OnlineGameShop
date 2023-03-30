1f242aa4e462417c5e6e6855c340244e22d85373
lucky-ducky-me <vlad01ovchinnikov02@gmail.com>
2023-03-30 12:32:10 +0300

    Добавлены свойста для json сериализцаии ответа.

--- OnlineGameShopApi/OnlineGameShopApi/Program.cs
+++ OnlineGameShopApi/OnlineGameShopApi/Program.cs
@@ -2,7 +2,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+    options.JsonSerializerOptions.PropertyNamingPolicy = null;
+    options.JsonSerializerOptions.WriteIndented = true;
+}); 
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
