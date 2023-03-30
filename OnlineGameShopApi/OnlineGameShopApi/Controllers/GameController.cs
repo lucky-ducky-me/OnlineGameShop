ff1aef0e1be5beabb3b8859dabc83737910587e0
lucky-ducky-me <vlad01ovchinnikov02@gmail.com>
2023-03-30 12:32:41 +0300

    Добавлены Get методы для всех игр и игры по id.

---
+++ OnlineGameShopApi/OnlineGameShopApi/Controllers/GameController.cs
@@ -0,0 +1,43 @@
+﻿using DataBaseProvider;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq.Expressions;
+
+namespace OnlineGameShopApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GameController : ControllerBase
+    {
+        //todo вынести строку в конфиг файл
+        private IOnlineGameShopProvider _onlineGameShopProvider
+            = new OnlineGameShopProvider("Server=DESKTOP-N04FOJI;Database=OnlineGameShop;Trusted_Connection=True;TrustServerCertificate=true");
+
+        //todo изменинть на класс модели, а не на класс сущности бд
+        [HttpGet]
+        public ActionResult<IEnumerable<DataBaseProvider.Models.Game>> Games()
+        { 
+            try
+            {
+                return _onlineGameShopProvider.GetAllGames().ToArray();
+            }
+            catch (Exception ex)
+            {
+                return NoContent();
+            }
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<DataBaseProvider.Models.Game> Game(Guid id)
+        {
+            try
+            {
+                return _onlineGameShopProvider.GetGame(id);
+            }
+            catch (Exception ex) 
+            {
+                return NoContent();
+            }
+        }
+    }
+}
